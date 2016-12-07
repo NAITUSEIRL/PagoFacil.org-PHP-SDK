e1920a3375cc406044651e4a23dac73c2c2116f3
Cristian Tala Sanchez <yomismo@cristiantala.cl>
2016-12-07 13:34:58 +0100

    Agregando Templates

--- classes/Resultado.php
+++ classes/Resultado.php
@@ -14,23 +14,56 @@ namespace ctala\transaccion\classes;
  * @author ctala
  */
 class Resultado {
+    /* @var $response Response */
 
-    public function pedidoRecibidoResponse(Response $response) {
-        $fields = array(
-            "Orden de compra Mall" => $response->ct_order_id_mall,
-            "Código de autorización" => $response->ct_authorization_code,
-            "Tipo de Pago" => $response->ct_payment_type_code,
-            "Monto" => $response->ct_monto,
-            "Final Tarjeta" => $response->ct_shares_number,
-            "Cuotas" => $response->ct_shares_number,
-            "Fecha Contable" => $response->ct_accounting_date,
-            "Fecha Transacción" => $response->ct_transaction_date,
+    public $response;
+    public $order_id;
+    public $order_id_mall;
+    public $fields;
+
+    function __construct() {
+        
+    }
+
+    function setResponse($response) {
+        $this->response = $response;
+        $this->fields = array(
+            "Orden de compra Mall" => $this->response->ct_order_id_mall,
+            "Código de autorización" => $this->response->ct_authorization_code,
+            "Tipo de Pago" => $this->response->ct_payment_type_code,
+            "Monto" => $this->response->ct_monto,
+            "Final Tarjeta" => $this->response->ct_shares_number,
+            "Cuotas" => $this->response->ct_shares_number,
+            "Fecha Contable" => $this->response->ct_accounting_date,
+            "Fecha Transacción" => $this->response->ct_transaction_date,
         );
+        $this->setOrder_id($this->response->ct_order_id);
+        $this->setOrder_id_mall($this->response->ct_order_id_mall);
+        
+    }
+
+    function setOrder_id($order_id) {
+        $this->order_id = $order_id;
+    }
+
+    function setOrder_id_mall($order_id_mall) {
+        $this->order_id_mall = $order_id_mall;
+    }
+
+    public function pedidoRecibidoResponse() {
+        $fields = $this->fields;
         include '../templates/orden_recibida.php';
     }
-    
-    public function pedidoFallido($order_id,$order_id_mall) {
+
+    public function pedidoFallido() {
+        $order_id = $this->order_id;
+        $order_id_mall = $this->order_id_mall;
+
         include '../templates/orden_fallida.php';
+    }
+
+    function setFields($fields) {
+        $this->fields = $fields;
     }
 
 }
